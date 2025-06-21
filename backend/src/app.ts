8f1552633d6f43b8d10a97385437a76f638bbf56
Revansa Helsa Kuswana <revansakuswana@gmail.com>
2025-06-21 14:20:38 +0700

    refactor: update app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.APP_BASE_URL,
+    origin: "https://test-bithealth.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,6 @@ mongoose
     app.listen(PORT, () => {
       console.log(`Server running at http://localhost:${PORT}`);
     });
-    console.log("Current NODE_ENV:", process.env.NODE_ENV);
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
