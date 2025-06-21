f2aeed43de45a97e8f9cebc776bca8057f6e0179
Revansa Helsa Kuswana <revansakuswana@gmail.com>
2025-06-21 15:13:47 +0700

    finalize to deploy backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -34,6 +34,10 @@ app.get("/health", async (req, res) => {
   });
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
